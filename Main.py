eec1afb853295ceb198b3b040a7ece16502cd599
pazfrank070-EE <pazfrank070@gmail.com>
2026-03-13 13:26:26 -0600

    Updated 3

--- Main.py
+++ Main.py
@@ -62,7 +62,7 @@ while True:
     pass"""
 
 
-from machine import Pin, PWM
+"""from machine import Pin, PWM
 
 # Crear PWM en los pines GPIO 2, 3, 4 y 5
 pwm2 = PWM(Pin(2))
@@ -85,5 +85,23 @@ pwm4.duty_u16(duty)
 pwm5.duty_u16(duty)
 
 # Mantener el programa corriendo
+while True:
+    pass """
+
+from machine import Pin
+
+# Configure GPIO 2, 3, 4, 5 as digital outputs
+pin2 = Pin(2, Pin.OUT)
+pin3 = Pin(3, Pin.OUT)
+pin4 = Pin(4, Pin.OUT)
+pin5 = Pin(5, Pin.OUT)
+
+# Set constant HIGH voltage
+pin2.value(1)
+pin3.value(1)
+pin4.value(1)
+pin5.value(1)
+
+# Keep program running
 while True:
     pass
