c3d962b414561d8dd6cc71de1ca0380f14dd6273
pazfrank070-EE <pazfrank070@gmail.com>
2026-03-13 13:36:47 -0600

    test 2

--- Main.py
+++ Main.py
@@ -62,7 +62,7 @@ while True:
     pass"""
 
 
-from machine import Pin, PWM
+"""from machine import Pin, PWM
 
 # Crear PWM en los pines GPIO 2, 3, 4 y 5
 pwm2 = PWM(Pin(2))
@@ -86,7 +86,7 @@ pwm5.duty_u16(duty)
 
 # Mantener el programa corriendo
 while True:
-    pass 
+    pass """
 
 """from machine import Pin
 
@@ -105,3 +105,10 @@ pin5.value(1)
 # Keep program running
 while True:
     pass"""
+
+from machine import Pin
+
+Pin(2, Pin.OUT).value(1)
+Pin(3, Pin.OUT).value(1)
+Pin(4, Pin.OUT).value(1)
+Pin(5, Pin.OUT).value(1)
