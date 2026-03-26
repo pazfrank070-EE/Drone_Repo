b9525f7dba2156d191a72277d685bf181dc1ff63
pazfrank070-EE <pazfrank070@gmail.com>
2026-03-26 14:44:45 -0600

    test code 2

--- Main.py
+++ Main.py
@@ -1,42 +1,33 @@
 from machine import Pin, PWM
 import time
 
-# Set PWM frequency (50 Hz for ESCs)
-FREQ = 50
+# Very slow frequency (1 Hz)
+FREQ = 1
 
-# Initialize PWM on pins
 esc_pins = [6, 7, 8, 9]
-escs = []
+pwms = []
 
 for pin in esc_pins:
     pwm = PWM(Pin(pin))
     pwm.freq(FREQ)
-    escs.append(pwm)
+    pwms.append(pwm)
 
-# Function to set throttle (0.0 to 1.0)
-def set_throttle(esc, throttle):
-    # Map throttle to duty cycle (1ms to 2ms pulse)
-    min_duty = 1638   # ~1ms
-    max_duty = 8192   # ~2ms
-    duty = int(min_duty + (max_duty - min_duty) * throttle)
-    esc.duty_u16(duty)
+# Set duty cycle (0 to 65535)
+def set_duty_all(duty):
+    for pwm in pwms:
+        pwm.duty_u16(duty)
 
-# ARM ESCs (IMPORTANT!)
-print("Arming ESCs...")
-for esc in escs:
-    set_throttle(esc, 0)  # minimum throttle
+print("Starting slow PWM demo...")
 
-time.sleep(3)  # wait for ESC to arm
+while True:
+    # 25% duty (short ON, long OFF)
+    set_duty_all(16384)
+    time.sleep(3)
 
-# TEST: ramp motors slowly
-print("Starting motors...")
-for t in range(0, 101):
-    throttle = t / 100
-    for esc in escs:
-        set_throttle(esc, throttle)
-    time.sleep(0.05)
+    # 50% duty (equal ON/OFF)
+    set_duty_all(32768)
+    time.sleep(3)
 
-# Stop motors
-print("Stopping motors...")
-for esc in escs:
-    set_throttle(esc, 0)
+    # 75% duty (long ON, short OFF)
+    set_duty_all(49152)
+    time.sleep(3)
